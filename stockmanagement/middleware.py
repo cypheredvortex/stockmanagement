30caa809017b33ba5ed728955934a6d45046cbf7
cypheredvortex <alimalik21realms@gmail.com>
2025-04-26 13:42:22 +0100

    Create middleware.py

---
+++ stockmanagement/middleware.py
@@ -0,0 +1,11 @@
+# stockmanagement/middleware.py
+
+from django.utils.deprecation import MiddlewareMixin
+
+class NoCacheMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
+
