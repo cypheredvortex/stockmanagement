50b5ef79a3c432f693f0a0d4303d8c59d6c112a8
cypheredvortex <alimalik21realms@gmail.com>
2025-04-26 12:59:11 +0100

    Update models.py

--- article/models.py
+++ article/models.py
@@ -1,10 +1,36 @@
 from django.db import models
-from fournisseur.models import Fournisseur
+from django.apps import apps
+
 # Create your models here.
 class Article(models.Model):
     id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=20)
-    reference = models.CharField(max_length=20)
-    prix = models.DecimalField(max_digits=10, decimal_places=2)
-    quantiteInitiale = models.IntegerField()
-    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE,null=True, blank=True)
+    references = models.CharField(max_length=20, null=True, blank=True)
+    categorie = models.CharField(max_length=20, null=True, blank=True)
+    prix = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    quantite = models.IntegerField(default=0)
+    
+    # Dynamically fetch the Fournisseur model to avoid circular import
+    fournisseur = models.ForeignKey('fournisseur.Fournisseur', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.nom
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+    def save(self, *args, **kwargs):    
+        super().save(*args, **kwargs)
+
+        if self.quantite < 10:
+            print('Stock faible')
+
+        if self.quantite == 0:
+            print('Stock vide')
+
+        if self.quantite > 100:
+            print('Stock suffisant')
+
+        if self.quantite > 50 and self.quantite < 100:
+            print('Stock moyen')
