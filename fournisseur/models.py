db7a1afc25dca574938280f55679c970bbafb066
cypheredvortex <alimalik21realms@gmail.com>
2025-04-26 13:01:57 +0100

    Update models.py

--- fournisseur/models.py
+++ fournisseur/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Fournisseur(models.Model):
-    id = models.AutoField(primary_key=True)  # Use AutoField instead of IntegerField
+    id = models.AutoField(primary_key=True)  
     nom = models.CharField(max_length=20)
     contact = models.CharField(max_length=20)
     adresse = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.nom)
