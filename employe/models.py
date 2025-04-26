b59d2625eeb406f1628b2a71b2b98063382cfa59
cypheredvortex <alimalik21realms@gmail.com>
2025-04-26 13:01:21 +0100

    Update models.py

--- employe/models.py
+++ employe/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class Employe(models.Model):
     id=models.AutoField(primary_key=True)
-    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, unique=True, null=True, blank=True)
