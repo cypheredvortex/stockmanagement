1f798cc80f46677b9e3b8b31dd2f2c9f1bd0732c
cypheredvortex <alimalik21realms@gmail.com>
2025-05-03 18:12:31 +0100

    Update views.py

--- rapport/views.py
+++ rapport/views.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from django.apps import apps
-
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
@@ -20,28 +19,31 @@ def disable_cache(response):
 @never_cache
 @login_required(login_url='/loginpage/')
 def create_rapport(request):
-    if not request.user.is_authenticated:
-        return redirect('/loginpage/')
-
     if request.method == 'POST':
         stock_id = request.POST.get('stockId')
-        date_rapport = request.POST.get('DateRapport')
-        mouvement_de_stock = request.POST.get('MouvementDeStock')
-        etat_stock = request.POST.get('EtatStock')
-        vente = request.POST.get('Vente')
-        approvisionnement = request.POST.get('Approvisionnement')
+        date_rapport = request.POST.get('dateRapport')
+        mouvement_de_stock = request.POST.get('mouvementDeStock')
+        etat_stock = request.POST.get('etatStock')
+        vente = request.POST.get('vente')
+        approvisionnement = request.POST.get('approvisionnement')
+        type_rapport = request.POST.get('type_rapport')
+        contenu = request.POST.get('contenu')
 
         try:
             stock_id = int(stock_id)
             date_rapport = datetime.strptime(date_rapport, '%Y-%m-%d').date()
 
+            # Create a new report with the current user assigned to 'généré_par'
             Rapport.objects.create(
                 stockId=stock_id,
-                DateRapport=date_rapport,
-                MouvementDeStock=mouvement_de_stock,
-                EtatStock=etat_stock,
-                Vente=vente,
-                Approvisionnement=approvisionnement
+                dateRapport=date_rapport,
+                mouvementDeStock=mouvement_de_stock,
+                etatStock=etat_stock,
+                vente=vente,
+                approvisionnement=approvisionnement,
+                type_rapport=type_rapport,
+                contenu=contenu,
+                généré_par=request.user,  # Set the current user as the creator
             )
 
             return redirect('get_rapports')
@@ -55,12 +57,10 @@ def create_rapport(request):
     response = render(request, 'rapport_form.html')
     return disable_cache(response)
 
+
 @never_cache
 @login_required(login_url='/loginpage/')
 def get_rapports(request):
-    if not request.user.is_authenticated:
-        return redirect('/loginpage/')
-
     rapports = Rapport.objects.all()
     response = render(request, 'rapports.html', {'rapports': rapports})
     return disable_cache(response)
@@ -68,20 +68,19 @@ def get_rapports(request):
 @never_cache
 @login_required(login_url='/loginpage/')
 def update_rapport(request, rapport_id):
-    if not request.user.is_authenticated:
-        return redirect('/loginpage/')
-
     rapport = get_object_or_404(Rapport, id=rapport_id)
 
     if request.method == 'POST':
         rapport.stockId = request.POST.get('stockId')
-        rapport.DateRapport = request.POST.get('DateRapport')
-        rapport.MouvementDeStock = request.POST.get('MouvementDeStock')
-        rapport.EtatStock = request.POST.get('EtatStock')
-        rapport.Vente = request.POST.get('Vente')
-        rapport.Approvisionnement = request.POST.get('Approvisionnement')
+        rapport.dateRapport = request.POST.get('dateRapport')
+        rapport.mouvementDeStock = request.POST.get('mouvementDeStock')
+        rapport.etatStock = request.POST.get('etatStock')
+        rapport.vente = request.POST.get('vente')
+        rapport.approvisionnement = request.POST.get('approvisionnement')
+        rapport.type_rapport = request.POST.get('type_rapport')
+        rapport.contenu = request.POST.get('contenu')
         rapport.save()
-        return redirect('rapport_list')
+        return redirect('get_rapports')
 
     response = render(request, 'rapport_form.html', {'rapport': rapport})
     return disable_cache(response)
@@ -89,31 +88,27 @@ def update_rapport(request, rapport_id):
 @never_cache
 @login_required(login_url='/loginpage/')
 def delete_rapport(request, rapport_id):
-    if not request.user.is_authenticated:
-        return redirect('/loginpage/')
-
     rapport = get_object_or_404(Rapport, id=rapport_id)
 
     if request.method == 'POST':
         rapport.delete()
-        return redirect('rapport_list')
+        return redirect('get_rapports')
 
     response = render(request, 'rapport_confirm_delete.html', {'rapport': rapport})
     return disable_cache(response)
 
+
 @never_cache
 @login_required(login_url='/loginpage/')
 def get_rapport_form(request):
-    if not request.user.is_authenticated:
-        return redirect('/loginpage/')
-
+    # Retrieve the Rapport and Stock models dynamically
     Rapport = apps.get_model('rapport', 'Rapport')
     Stock = apps.get_model('stock', 'Stock')
 
     rapports = Rapport.objects.all()
     stocks = Stock.objects.all()
 
-    response = render(request, 'rapport_form.html', {  # <--- render to 'rapports_form.html'
+    response = render(request, 'rapport_form.html', {  # Render the form to add/edit a report
         'rapports': rapports,
         'stocks': stocks,
     })
@@ -128,15 +123,12 @@ def render_to_pdf(template_src, context_dict={}):
         return HttpResponse('Erreur lors de la génération du PDF')
     return response
 
+
 def rapport_pdf(request):
     context = {'nom': 'Yasser', 'produits': ['Article A', 'Article B']}
     return render_to_pdf('rapport_template.html', context)
 
 def rapport_pdf(request):
-    from django.template.loader import get_template
-    from django.http import HttpResponse
-    from xhtml2pdf import pisa
-
     rapports = Rapport.objects.all()
     template = get_template('rapport_pdf.html')
     html = template.render({'rapports': rapports})
