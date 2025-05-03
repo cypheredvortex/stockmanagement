bf10eadfeee3a52ce5f98d901540fc2b22eb5527
cypheredvortex <alimalik21realms@gmail.com>
2025-05-03 18:02:27 +0100

    Update models.py

--- commande/models.py
+++ commande/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 from fournisseur.models import Fournisseur
+from article.models import Article
 
-# Create your models here.
 class Commande(models.Model):
     id = models.AutoField(primary_key=True)
     fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE, null=True, blank=True)
     dateCommande = models.DateField()
-    statut = models.CharField(max_length=20)
+    etatCommande = models.CharField(
+        max_length=15,
+        choices=[("en_cours", "En cours"), ("complete", "Complete"), ("annule", "Annulé")],
+        default="en_cours"
+    )
+    prix = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
 
     def __str__(self):
-        return self.fournisseur.nom
-    
+        return f"Commande {self.id} - Fournisseur: {self.fournisseur.nom}"
+
+class ArticleCommande(models.Model):
+    commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    quantite = models.IntegerField()
+    prix_unitaire = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return f"Article: {self.article.nom} - Quantité: {self.quantite}"
